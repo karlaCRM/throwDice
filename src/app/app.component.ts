958404b098a539761f50df5abeefbfc7a5208589
karlaCRM <abraxaskrm@gmail.com>
2023-03-17 15:59:49 -0600

    dice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dices';
+
+  
+ diceLeft: string = "../assets/img/dice1.png";
+ diceRight: string = "../assets/img/dice4.png";
+  numberOne: number = 1;
+  numberTwo: number = 2;
+
+
+  throwDices(): void{
+    this.numberOne = Math.round(Math.random()*5) + 1;
+    this.numberTwo = Math.round(Math.random()*5) + 1;
+    
+    this.diceLeft = `../assets/img/dice${this.numberOne}.png`;
+    this.diceRight = `../assets/img/dice${this.numberTwo}.png`;
+    if(this.diceLeft === this.diceRight){
+      Swal.fire(
+        'You Win',
+        '⭐, 🔥'
+      )
+    }
+
+  }
+
 }
